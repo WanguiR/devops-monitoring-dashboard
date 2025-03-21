5f5b509280928c7a29ef909beee7bf516665a30b
wanguir <wanguikibiru22@gmail.com>
2025-03-21 10:54:51 +0300

    app update

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,17 @@
 from flask import Flask , render_template
 app = Flask(__name__)
+
+# sample ci/cd data
+
+data = {
+    "last_build_status": "Successful",
+    "build_time":"45 sec",
+    "deploy_status" :"deployed",
+    "container_count": 4
+}
+
+@app.route ('/')
+def dashboard ():
+    return render_template ('dashboard.html',data=data )
+if __name__=="__main__":
+    app.run (debug=True)
